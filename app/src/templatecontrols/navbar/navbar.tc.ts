928c682fffef7da8a490ce28303f1d3488fff2ab
Jackson Carr <jackson.r.carr@gmail.com>
2015-10-19 20:18:26 -0500

    preliminary navbar and footer; some styling included that is universal among all pages

---
+++ app/src/templatecontrols/navbar/navbar.tc.ts
@@ -0,0 +1,19 @@
+import {register, ui} from 'platypus';
+import BaseViewControl from '../../viewcontrols/base/base.vc';
+import HomeViewControl from '../../viewcontrols/home/home.vc';
+import AboutViewControl from '../../viewcontrols/about/about.vc';
+import ConnectViewControl from '../../viewcontrols/connect/connect.vc';
+import PortfolioViewControl from '../../viewcontrols/portfolio/portfolio.vc';
+
+export default class NavbarTemplateControl extends ui.TemplateControl {
+    templateString: string = require('./navbar.tc.html');
+    context: any = {
+        showNavbar: true,
+        homeView: HomeViewControl,
+        aboutView: AboutViewControl,
+        connectView: ConnectViewControl,
+        portfolioView: PortfolioViewControl
+    }
+}
+
+register.control('navbar', NavbarTemplateControl);
