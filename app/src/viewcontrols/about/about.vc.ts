f19b0bb9383413beec8a63f55a8f82f42bd8b8fc
acttechie55 <actaylor55@gmail.com>
2015-10-20 12:29:42 -0500

    actually resolved all of our merge conflicts - AT

--- app/src/viewcontrols/about/about.vc.ts
+++ app/src/viewcontrols/about/about.vc.ts
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-import {register} from 'platypus';
-=======
 import {register, ui} from 'platypus';
->>>>>>> a12dddb6ecd40c2bc8e23f31527f5a0326cde360
 import BaseViewControl from '../base/base.vc';
 
 export default class AboutViewControl extends BaseViewControl {
