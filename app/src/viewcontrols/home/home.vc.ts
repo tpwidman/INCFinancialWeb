20e93b627c1f9cf80819bdcd48013740ee8a9a72
Jackson Carr <jackson.r.carr@gmail.com>
2015-10-21 15:05:41 -0500

    some functionality in the main icons on home page

--- app/src/viewcontrols/home/home.vc.ts
+++ app/src/viewcontrols/home/home.vc.ts
@@ -5,8 +5,36 @@ import ConnectViewControl from '../connect/connect.vc';
 export default class HomeViewControl extends BaseViewControl {
     templateString: string = require('./home.vc.html');
 
-    context: any = {};
+    context: any = {
+        showCreate: false,
+        showGrow: false,
+        showKeep: false
+    };
 
+    expandCreate() {
+        this.context.showCreate = true
+    }
+    expandGrow() {
+        this.context.showGrow = true
+    }
+    expandKeep() {
+        this.context.showKeep = true
+    }
+    collapseCreate() {
+        if (this.context.showCreate = true) {
+            this.context.showCreate = false
+        }
+    }
+    collapseGrow() {
+        if (this.context.showGrow = true) {
+            this.context.showGrow = false
+        }
+    }
+    collapseKeep() {
+        if(this.context.showKeep = true) {
+            this.context.showKeep = false
+        }
+    }
 }
 
 register.viewControl('home-vc', HomeViewControl);
