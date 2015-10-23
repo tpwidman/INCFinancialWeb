5d5189888a87807ca0865331e56fa6eaae9be42d
Jackson Carr <jackson.r.carr@gmail.com>
2015-10-23 13:46:28 -0500

    fixed navigation issue in base.vc

--- app/src/viewcontrols/base/base.vc.ts
+++ app/src/viewcontrols/base/base.vc.ts
@@ -4,7 +4,7 @@ import {acquire, async, Document} from 'platypus';
 export default class BaseViewControl extends ui.ViewControl {
     protected document: Document = acquire(Document);
     protected Promise: async.IPromise = acquire(async.IPromise);
-    protected scrollingContainer: HTMLElement = <HTMLElement>this.document.body.firstElementChild;
+    protected scrollingContainer: HTMLElement = <HTMLElement>this.document.body;
 
     context: any = {};
 
