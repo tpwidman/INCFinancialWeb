989679c1d7a626eb12731946664718c71eb37fb4
Jackson Carr <jackson.r.carr@gmail.com>
2015-10-23 18:19:04 -0500

    still working on navbar -- we're in for a bumpy ride, but where we're going, we don't need roads. - JC

--- app/src/templatecontrols/navbar/navbar.tc.ts
+++ app/src/templatecontrols/navbar/navbar.tc.ts
@@ -1,4 +1,4 @@
-import {register, ui} from 'platypus';
+import {controls, events, register, ui} from 'platypus';
 import BaseViewControl from '../../viewcontrols/base/base.vc';
 import HomeViewControl from '../../viewcontrols/home/home.vc';
 import AboutViewControl from '../../viewcontrols/about/about.vc';
@@ -7,6 +7,7 @@ import PortfolioViewControl from '../../viewcontrols/portfolio/portfolio.vc';
 
 export default class NavbarTemplateControl extends ui.TemplateControl {
     templateString: string = require('./navbar.tc.html');
+    navbar: controls.INamedElement<HTMLDivElement, any>;
     context: any = {
         showNavbar: true,
         homeView: HomeViewControl,
@@ -15,12 +16,19 @@ export default class NavbarTemplateControl extends ui.TemplateControl {
         portfolioView: PortfolioViewControl
     }
 
-    onScroll() : void {
-        if (document.body.scrollTop > 50) {
-            document.getElementById("nav").className = "background-transparent";
-        } else {
-            document.getElementById("nav").className = "background-opaque";
-        }
+    loaded() : void {
+        let scrollNav: HTMLElement = document.body,
+           navbar = this.navbar.element;
+
+        document.addEventListener('scroll', (e) => {
+            var elem: HTMLElement = <HTMLElement>e.target;
+            if (document.body.scrollTop >= 25) {
+                console.log('adding class');
+                document.body.;
+            } else {
+                ('background: transparent');
+            }
+        });
     }
 }
 
