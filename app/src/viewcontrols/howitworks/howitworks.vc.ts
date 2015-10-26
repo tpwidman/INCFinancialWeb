51bdde985e5395af5e4637526aa65fc20fd3f136
Jackson Carr <jackson.r.carr@gmail.com>
2015-10-26 10:36:19 -0500

    some minor changes, added howitworks vc

---
+++ app/src/viewcontrols/howitworks/howitworks.vc.ts
@@ -0,0 +1,53 @@
+import {register} from 'platypus';
+import BaseViewControl from '../base/base.vc';
+import ConnectViewControl from '../connect/connect.vc';
+
+export default class HowitworksViewControl extends BaseViewControl {
+    templateString: string = require('./howitworks.vc.html');
+
+    context: any = {
+        showCreate: false,
+        showGrow: false,
+        showKeep: false
+    };
+
+    expandCreate() {
+        if(this.context.showCreate === true) {
+            this.context.showCreate = false;
+        } else if(this.context.showCreate === false) {
+            this.context.showCreate = true;
+        }
+    }
+    expandGrow() {
+        if(this.context.showGrow === true) {
+            this.context.showGrow = false;
+        } else if(this.context.showGrow === false) {
+            this.context.showGrow = true;
+        }
+    }
+    expandKeep() {
+        if(this.context.showKeep === true) {
+            this.context.showKeep = false;
+        } else if(this.context.showKeep === false) {
+            this.context.showKeep = true;
+        }
+    }
+
+    collapseCreate() {
+        if (this.context.showCreate = true) {
+            this.context.showCreate = false;
+        }
+    }
+    collapseGrow() {
+        if (this.context.showGrow = true) {
+            this.context.showGrow = false;
+        }
+    }
+    collapseKeep() {
+        if(this.context.showKeep = true) {
+            this.context.showKeep = false
+        }
+    }
+}
+
+register.viewControl('howitworks-vc', HowitworksViewControl);
